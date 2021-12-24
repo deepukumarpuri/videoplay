65152186f4c65b79eea1d13a2e87a53ac6d0cedd
deepukumarpuri <76464956+deepukumarpuri@users.noreply.github.com>
2021-12-24 08:19:53 +0530

    Update commands.py

--- program/owner/commands.py
+++ program/owner/commands.py
@@ -7,11 +7,10 @@ from pyrogram import Client, filters
 from pyrogram.errors.exceptions.bad_request_400 import ChatAdminRequired
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from database.ia_filterdb import Media, get_file_details, unpack_new_file_id
-from driver.database import db
+from program.database import db
 from config import ADMINS, AUTH_CHANNEL, LOG_CHANNEL, PICS, OWNER_NAME
 from utils import get_size, is_subscribed, temp
 import re
-from program import __version__
 from program.decorators import sudo_users_only
 from program.filters import command
 from pyrogram import Client, filters
