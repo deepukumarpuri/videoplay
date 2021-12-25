570c87d3ad455ef85b3eedd31345ae7e27b128d2
deepukumarpuri <76464956+deepukumarpuri@users.noreply.github.com>
2021-12-25 07:44:20 +0530

    Update commands.py

--- program/owner/commands.py
+++ program/owner/commands.py
@@ -93,8 +93,7 @@ async def start(client, message):
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
             photo=random.choice(PICS),
-            caption=Script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
-            quote=True,
             parse_mode='html'
         )
