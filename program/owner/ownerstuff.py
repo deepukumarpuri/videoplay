9be9cc42bae280f5ff701a15cb937e43de855e0b
deepukumarpuri <76464956+deepukumarpuri@users.noreply.github.com>
2021-12-25 11:06:41 +0530

    Add files via upload

---
+++ program/owner/ownerstuff.py
@@ -0,0 +1,172 @@
+# Credits @AbirHasan2005, @DevsExpo and DaisyXMusic
+# This file is part of CallsMusic-Plus (https://github.com/Itz-fork/Callsmusic-Plus)
+
+import sys
+import os
+import heroku3
+import time
+import traceback
+import asyncio
+import shutil
+import psutil
+
+from pyrogram import Client, filters
+from pyrogram.types import Message, Dialog, Chat
+from pyrogram.errors import UserAlreadyParticipant
+from datetime import datetime
+from functools import wraps
+from os import environ, execle, path, remove
+from git import Repo
+from git.exc import GitCommandError, InvalidGitRepositoryError, NoSuchPathError
+
+from driver.veez import user as pakaya
+from driver.database import db
+from driver.dbthings import main_broadcast_handler
+from driver.musicdwn import humanbytes, get_text
+from config import BOT_USERNAME, SUDO_USERS
+
+
+# Stats Of Your Bot
+@Client.on_message(filters.command("stat") & filters.user(SUDO_USERS))
+async def botstats(_, message: Message):
+    total, used, free = shutil.disk_usage(".")
+    total = humanbytes(total)
+    used = humanbytes(used)
+    free = humanbytes(free)
+    cpu_usage = psutil.cpu_percent()
+    ram_usage = psutil.virtual_memory().percent
+    disk_usage = psutil.disk_usage('/').percent
+    total_users = await db.total_users_count()
+    await message.reply_text(
+        text=f"**💫 Bot Stats Of @{BOT_USERNAME} 💫** \n\n**🤖 Bot Version:** `V2.9.1` \n\n**👥 Users:** \n ↳**PM'ed Users:** `{total_users}` \n\n**💾 Disk Usage,** \n ↳**Total Disk Space:** `{total}` \n ↳**Used:** `{used}({disk_usage}%)` \n ↳**Free:** `{free}` \n\n**🎛 Hardware Usage,** \n ↳**CPU Usage:** `{cpu_usage}%` \n ↳**RAM Usage:** `{ram_usage}%`",
+        parse_mode="Markdown",
+        quote=True
+    )
+
+
+# Broadcast message to users (This will Broadcast using Bot with Db)
+@Client.on_message(filters.private & filters.command("cast") & filters.user(SUDO_USERS) & filters.reply)
+async def broadcast_handler_open(_, m: Message):
+    await main_broadcast_handler(m, db)
+
+# Broadcast message to users (This will Broadcast using streamer account without db)
+@Client.on_message(filters.command(["chatcast"]))
+async def chatcast(_, message: Message):
+    sent=0
+    failed=0
+    if message.from_user.id not in SUDO_USERS:
+        await message.reply("Go away! This is not for you 😂!")
+        return
+    else:
+        wtf = await message.reply("`Starting a Chatcast...`")
+        if not message.reply_to_message:
+            await wtf.edit("Please Reply to a Message to Chatcast it 🥺!")
+            return
+        lmao = message.reply_to_message.text
+        async for dialog in pakaya.iter_dialogs():
+            try:
+                await pakaya.send_message(dialog.chat.id, lmao)
+                sent = sent+1
+                await wtf.edit(f"`ChatCasting...` \n\n**Sent to:** `{sent}` Chats \n**Failed in:** {failed} Chats")
+            except:
+                failed=failed+1
+                await wtf.edit(f"`ChatCasting...` \n\n**Sent to:** `{sent}` Chats \n**Failed in:** {failed} Chats")
+            await asyncio.sleep(3)
+        await message.reply_text(f"`ChatCasting Finished 😌` \n\n**Sent to:** `{sent}` Chats \n**Failed in:** {failed} Chats")
+
+
+# Ban User
+@Client.on_message(filters.private & filters.command("bans") & filters.user(SUDO_USERS))
+async def ban(c: Client, m: Message):
+    if len(m.command) == 1:
+        await m.reply_text(
+            f"Use this command to ban Users from using this bot 🤒! Read __**/modhelp**__ to Learn how to use this 🤭!",
+            quote=True
+        )
+        return
+    try:
+        user_id = int(m.command[1])
+        ban_duration = int(m.command[2])
+        ban_reason = ' '.join(m.command[3:])
+        ban_log_text = f"`Banning User 🗑...` \nUser ID: `{user_id}` \nDuration: `{ban_duration}` \nReason: `{ban_reason}`"
+        try:
+            await c.send_message(
+                user_id,
+                f"Lmao You are **Banned 😂!** \n\nReason: `{ban_reason}` \nDuration: `{ban_duration}` day(s). \n\n**Message From The Owner! Ask in **@Nexa_bots** if you think this was an mistake."
+            )
+            ban_log_text += '\n\nSuccessfully Notified About This Ban to that **Dumb User** 😅'
+        except:
+            traceback.print_exc()
+            ban_log_text += f"\n\nKCUF! I can't Notify About This Ban to That **Dumb User** 🤯 \n\n`{traceback.format_exc()}`"
+        await db.ban_user(user_id, ban_duration, ban_reason)
+        print(ban_log_text)
+        await m.reply_text(
+            ban_log_text,
+            quote=True
+        )
+    except:
+        traceback.print_exc()
+        await m.reply_text(
+            f"An Error Occoured ❌! Traceback is given below\n\n`{traceback.format_exc()}`",
+            quote=True
+        )
+
+
+# Unban User
+@Client.on_message(filters.private & filters.command("unbans") & filters.user(SUDO_USERS))
+async def unban(c: Client, m: Message):
+    if len(m.command) == 1:
+        await m.reply_text(
+            f"Use this command to ban Users from using this bot 🤒! Read __**/modhelp**__ to Learn how to use this 🤭!",
+            quote=True
+        )
+        return
+    try:
+        user_id = int(m.command[1])
+        unban_log_text = f"`Unbanning user...` /n**User ID:**{user_id}"
+        try:
+            await c.send_message(
+                user_id,
+                f"Good News! **You are Unbanned** 😊!"
+            )
+            unban_log_text += '\n\nSuccessfully Notified About This to that **Good User** 😅'
+        except:
+            traceback.print_exc()
+            unban_log_text += f"\n\nKCUF! I can't Notify About This to That **Dumb User** 🤯 \n\n`{traceback.format_exc()}`"
+        await db.remove_ban(user_id)
+        print(unban_log_text)
+        await m.reply_text(
+            unban_log_text,
+            quote=True
+        )
+    except:
+        traceback.print_exc()
+        await m.reply_text(
+            f"An Error Occoured ❌! Traceback is given below\n\n`{traceback.format_exc()}`",
+            quote=True
+        )
+
+
+# Banned User List
+@Client.on_message(filters.private & filters.command("banslist") & filters.user(SUDO_USERS))
+async def _banned_usrs(_, m: Message):
+    all_banned_users = await db.get_all_banned_users()
+    banned_usr_count = 0
+    text = ''
+    async for banned_user in all_banned_users:
+        user_id = banned_user['id']
+        ban_duration = banned_user['ban_status']['ban_duration']
+        banned_on = banned_user['ban_status']['banned_on']
+        ban_reason = banned_user['ban_status']['ban_reason']
+        banned_usr_count += 1
+        text += f"➬ **User ID**: `{user_id}`, **Ban Duration**: `{ban_duration}`, **Banned Date**: `{banned_on}`, **Ban Reason**: `{ban_reason}`\n\n"
+    reply_text = f"**Total Banned:** `{banned_usr_count}`\n\n{text}"
+    if len(reply_text) > 4096:
+        with open('banned-user-list.txt', 'w') as f:
+            f.write(reply_text)
+        await m.reply_document('banned-user-list.txt', True)
+        os.remove('banned-user-list.txt')
+        return
+    await m.reply_text(reply_text, True)
+
+
