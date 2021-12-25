dc94cea0268b36703f9c9ed81e59e62f4cb0386e
deepukumarpuri <76464956+deepukumarpuri@users.noreply.github.com>
2021-12-25 15:01:52 +0530

    Update p_ttishow.py

--- program/owner/p_ttishow.py
+++ program/owner/p_ttishow.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.errors.exceptions.bad_request_400 import MessageTooLong, PeerIdInvalid
-from config import ADMINS, LOG_CHANNEL, SUPPORT_GROUP
+from config import ADMINS, LOG_CHANNEL, SUPPORT_GROUP, SUPPORT_CHAT
 from driver.database import db
 from database.ia_filterdb import Media
 from utils import get_size, temp
