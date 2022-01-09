bfe131ba80fdf9219180c737bb32beb917de27ad
deepukumarpuri <76464956+deepukumarpuri@users.noreply.github.com>
2022-01-09 10:54:37 +0530

    Add files via upload

--- program/owner/main.py
+++ program/owner/main.py
@@ -8,7 +8,7 @@ from pyrogram.errors.exceptions.bad_request_400 import ChatAdminRequired
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from database.ia_filterdb import Media, get_file_details, unpack_new_file_id
 from driver.database import db, Database
-from config import ADMINS, LOG_CHANNEL, PICS
+from config import ADMINS, LOG_CHANNEL, PICS, VIDEO_LINK
 from utils import get_size, is_subscribed, temp
 import re
 logger = logging.getLogger(__name__)
@@ -18,7 +18,7 @@ async def start(client, message):
     if message.chat.type in ['group', 'supergroup']:
         buttons = [
             [
-                InlineKeyboardButton("❓ Basic Guide", callback_data="cbhowtouse")
+                InlineKeyboardButton("📹 Tutorial Video", url=f"{VIDEO_LINK}")
             ],
             [
                 InlineKeyboardButton("📚 Commands", callback_data="cbcmds"),
@@ -51,13 +51,64 @@ async def start(client, message):
             InlineKeyboardButton("👥 Official Group", url=f"https://t.me/{GROUP_SUPPORT}")
             ],[
             InlineKeyboardButton("📣 Official Channel", url=f"https://t.me/{UPDATES_CHANNEL}"),
-            InlineKeyboardButton("📹 Tutorial Video", url=f"https://t.me/{UPDATES_CHANNEL}")
+            InlineKeyboardButton("📹 Tutorial Video", url=f"{VIDEO_LINK}")
             ],[
             InlineKeyboardButton('⌦ Close the Menu ⌫', callback_data='close_data')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_text(
             text=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+            disable_web_page_preview=False,
+            reply_markup=reply_markup,
+            quote=True,
+            parse_mode='html'
+        )
+
+@Client.on_message(filters.command("vcs"))
+async def start(client, message):
+    if message.chat.type in ['group', 'supergroup']:
+        buttons = [
+            [
+                InlineKeyboardButton("➕ Add me to your Group ➕", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
+            ],
+            [
+                InlineKeyboardButton("📹 Tutorial Video", callback_data="dkvideo"),
+                InlineKeyboardButton('❤️ Donate', url='https://t.me/DKBOTZHELP')
+            ],
+            [
+                InlineKeyboardButton('⌦ Close the Menu ⌫', callback_data="close_data"),
+            ]
+            ]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await message.reply(script.G_VC_TXT.format(message.from_user.mention if message.from_user else message.chat.title, message.chat.username), reply_markup=reply_markup)
+        await asyncio.sleep(2) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
+        if not await db.get_chat(message.chat.id):
+            total=await client.get_chat_members_count(message.chat.id)
+            await client.send_message(LOG_CHANNEL, script.LOG_TEXT_G.format(message.chat.title, message.chat.id, total, "Unknown"))       
+            await db.add_chat(message.chat.id, message.chat.title)
+        return 
+    if not await db.is_user_exist(message.from_user.id):
+        await db.add_user(message.from_user.id, message.from_user.first_name)
+        await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
+    if len(message.command) != 2:
+        buttons = [[
+            InlineKeyboardButton("➕ Add me to your Group ➕", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
+            ],[
+            InlineKeyboardButton("📹 All Tutorial Video", callback_data="dkvideo"),
+            InlineKeyboardButton("📚 Commands", callback_data="started")
+            ],[
+            InlineKeyboardButton('❤️ Donate', url='https://t.me/DKBOTZHELP')
+            ],[
+            InlineKeyboardButton("👥 Official Group", url=f"https://t.me/{GROUP_SUPPORT}")
+            ],[
+            InlineKeyboardButton("📣 Official Channel", url=f"https://t.me/{UPDATES_CHANNEL}"),
+            InlineKeyboardButton("📹 Tutorial Video", url=f"{VIDEO_LINK}")
+            ],[
+            InlineKeyboardButton('⌦ Close the Menu ⌫', callback_data='close_data')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await message.reply_text(
+            text=script.VC_TXT.format(message.from_user.mention),
             disable_web_page_preview=True,
             reply_markup=reply_markup,
             quote=True,
