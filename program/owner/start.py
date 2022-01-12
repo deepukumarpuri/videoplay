6690da5b530b1c79af52ee20bd338cce8721427e
deepukumarpuri <76464956+deepukumarpuri@users.noreply.github.com>
2022-01-12 07:12:50 +0530

    Update start.py

--- program/owner/start.py
+++ program/owner/start.py
@@ -1,5 +1,13 @@
 import os
 from datetime import datetime
+from script import Script as script
+from database.ia_filterdb import Media, get_file_details, unpack_new_file_id
+from driver.database import db, Database
+from config import *
+from utils import get_size, is_subscribed, temp
+import re
+logger = logging.getLogger(__name__)
+
 from sys import version_info
 from time import time
 from pyrogram import Client, filters # Ik this is weird as this shit is already imported in line 6! anyway ... Fuck Off!
@@ -9,7 +17,7 @@ from pytgcalls import (__version__ as pytover)
 from driver.filters import command, other_filters, other_filters2
 from driver.database import db, Database
 from driver.dbthings import handle_user_status
-from config import LOG_CHANNEL, BOT_USERNAME, UPDATES_CHANNEL, ALIVE_IMG, ALIVE_NAME, BOT_NAME, GROUP_SUPPORT, OWNER_NAME
+from config import *
 
 
 __major__ = 0
@@ -48,7 +56,58 @@ async def _(bot: Client, cmd: Message):
 
 
 
+logger = logging.getLogger(__name__)
 
+@Client.on_message(filters.command("start"))
+async def start(client, message):
+    if message.chat.type in ['group', 'supergroup']:
+        buttons = [
+            [
+                InlineKeyboardButton("➕ Add me to your Group ➕", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
+            ],
+            [
+                InlineKeyboardButton("📹 Tutorial Video", callback_data="dkvideo"),
+                InlineKeyboardButton('❤️ Donate', url='https://t.me/DKBOTZHELP')
+            ],
+            [
+                InlineKeyboardButton('⌦ Close the Menu ⌫', callback_data="close_data"),
+            ]
+            ]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await message.reply(script.G_VC_TXT.format(message.from_user.mention if message.from_user else message.chat.title, message.chat.username), reply_markup=reply_markup)
+        await asyncio.sleep(2) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
+        if not await db.get_chat(message.chat.id):
+            total=await client.get_chat_members_count(message.chat.id)
+            await client.send_message(LOG_CHANNEL, script.LOG_TEXT_G.format(message.chat.title, message.chat.id, total, "Unknown"))       
+            await db.add_chat(message.chat.id, message.chat.title)
+        return 
+    if not await db.is_user_exist(message.from_user.id):
+        await db.add_user(message.from_user.id, message.from_user.first_name)
+        await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
+    if len(message.command) != 2:
+        buttons = [[
+            InlineKeyboardButton("➕ Add me to your Group ➕", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
+            ],[
+            InlineKeyboardButton("📹 All Tutorial Video", callback_data="dkvideo"),
+            InlineKeyboardButton("📚 Commands", callback_data="cbcmds")
+            ],[
+            InlineKeyboardButton('❤️ Donate', url='https://t.me/DKBOTZHELP')
+            ],[
+            InlineKeyboardButton("👥 Official Group", url=f"https://t.me/{GROUP_SUPPORT}")
+            ],[
+            InlineKeyboardButton("📣 Official Channel", url=f"https://t.me/{UPDATES_CHANNEL}"),
+            InlineKeyboardButton("📹 Tutorial Video", url=f"{VIDEO_LINK}")
+            ],[
+            InlineKeyboardButton('⌦ Close the Menu ⌫', callback_data='close_data')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await message.reply_text(
+            text=script.VC_TXT.format(message.from_user.mention),
+            disable_web_page_preview=True,
+            reply_markup=reply_markup,
+            quote=True,
+            parse_mode='html'
+        )
 
 
 # Help Menu
