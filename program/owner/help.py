52ae2135068ded265a9471147dd0cc1e55e28914
deepukumarpuri <76464956+deepukumarpuri@users.noreply.github.com>
2022-01-13 07:47:23 +0530

    Update help.py

--- program/owner/help.py
+++ program/owner/help.py
@@ -1,1 +1,77 @@
-H
+import os
+import logging
+import random
+import asyncio
+from datetime import datetime
+from script import Script as script
+from database.ia_filterdb import Media, get_file_details, unpack_new_file_id
+from driver.database import db, Database
+from config import *
+from utils import get_size, is_subscribed, temp
+import re
+logger = logging.getLogger(__name__)
+from sys import version_info
+from time import time
+from pyrogram import Client, filters # Ik this is weird as this shit is already imported in line 6! anyway ... Fuck Off!
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, Chat
+from pyrogram import __version__ as pyrover
+from pytgcalls import (__version__ as pytover)
+from driver.filters import command, other_filters, other_filters2
+from driver.database import db, Database
+from driver.dbthings import handle_user_status
+from config import *
+
+
+
+
+@Client.on_message(filters.command("help"))
+async def start(client, message):
+    if message.chat.type in ['group', 'supergroup']:
+        buttons = [[
+            InlineKeyboardButton("🤔 How To Use Me 🤔", callback_data="cbhowtouse")
+            ],[
+            InlineKeyboardButton("👷🏻 Admin Command", callback_data="cbadmin"),
+            InlineKeyboardButton("📚 Basic Command", callback_data="cbbasic")
+            ],[
+            InlineKeyboardButton("🧙🏻 Sudo Command", callback_data="cbsudo"),
+            InlineKeyboardButton("👨‍💻 Owner Command", callback_data="cbowner")
+            ],[
+            InlineKeyboardButton("📣 Official Channel", url=f"https://t.me/{UPDATES_CHANNEL}"),
+            InlineKeyboardButton("👥 Official Group", url=f"https://t.me/{GROUP_SUPPORT}")
+            ],[
+            InlineKeyboardButton('⌦ Close the Menu ⌫', callback_data='close_data')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await message.reply(script.GROUP_START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, message.chat.username), reply_markup=reply_markup)
+        await asyncio.sleep(2) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
+        if not await db.get_chat(message.chat.id):
+            total=await client.get_chat_members_count(message.chat.id)
+            await client.send_message(LOG_CHANNEL, script.LOG_TEXT_G.format(message.chat.title, message.chat.id, total, "Unknown"))       
+            await db.add_chat(message.chat.id, message.chat.title)
+        return 
+    if not await db.is_user_exist(message.from_user.id):
+        await db.add_user(message.from_user.id, message.from_user.first_name)
+        await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
+    if len(message.command) != 2:
+        buttons = [[
+            InlineKeyboardButton("🤔 How To Use Me 🤔", callback_data="cbhowtouse")
+            ],[
+            InlineKeyboardButton("👷🏻 Admin Command", callback_data="cbadmin"),
+            InlineKeyboardButton("📚 Basic Command", callback_data="cbbasic")
+            ],[
+            InlineKeyboardButton("🧙🏻 Sudo Command", callback_data="cbsudo"),
+            InlineKeyboardButton("👨‍💻 Owner Command", callback_data="cbowner")
+            ],[
+            InlineKeyboardButton("📣 Official Channel", url=f"https://t.me/{UPDATES_CHANNEL}"),
+            InlineKeyboardButton("👥 Official Group", url=f"https://t.me/{GROUP_SUPPORT}")
+            ],[
+            InlineKeyboardButton('⌦ Close the Menu ⌫', callback_data='close_data')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await message.reply_text(
+            text=script.HELP_TXT.format(message.from_user.mention),
+            disable_web_page_preview=True,
+            reply_markup=reply_markup,
+            quote=True,
+            parse_mode='html'
+        )
